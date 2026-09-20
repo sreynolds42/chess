8e9eea6f508864d5a6ea1312853404bc41c0a8b8
sreynolds42 <echo.github@zuka.com>
2026-09-19 21:16:48 -0600

    getRow and getColumn

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,22 +8,25 @@ package chess;
  */
 public class ChessPosition {
 
+    private final int row;
+    private final int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
      */
-    public int getRow() {
-        throw new RuntimeException("Not implemented");
-    }
+    public int getRow() { return row; }
 
     /**
      * @return which column this position is in
      * 1 codes for the left column
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }
